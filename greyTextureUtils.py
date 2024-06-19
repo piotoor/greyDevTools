05661716054af704c2d08eb1e4841c702e8cc2e8
Piotr Kozka <piotoor@gmail.com>
2024-06-19 11:43:45 +0200

    Added more debug printouts to the darkening lut generator

--- greyTextureUtils.py
+++ greyTextureUtils.py
@@ -72,23 +72,29 @@ def generate_texture_dark_transition_map(light, dark):
         debug[0].append((lo_nibble_light, lo_nibble_dark))
         debug[1].append((hi_nibble_light, hi_nibble_dark))
         debug[2].append((light_cr[i], dark_cr[i]))
+        greyLogger.debug("pixelId = {} {} -> {}   {} -> {}   {} -> {}".format(i, lo_nibble_light, lo_nibble_dark,
+                                                                              hi_nibble_light, hi_nibble_dark,
+                                                                              light_cr[i], dark_cr[i]))
+
+        common_err_msg = "Inconsistent light-dark texture relation. Can't generate a dark transition vector. " \
+            "Trying to assign {} to {}, but {} already has {} mapped. Pixel index = " + str(i)
 
         if ans[lo_nibble_light] == lo_nibble_light:
             ans[lo_nibble_light] = lo_nibble_dark
         elif ans[lo_nibble_light] != lo_nibble_dark:
-            raise RuntimeError("Inconsistent light-dark texture relation. Can't generate a dark transition vector")
+            raise RuntimeError(common_err_msg.format(lo_nibble_dark, lo_nibble_light, lo_nibble_light, ans[lo_nibble_light]))
 
         if ans[hi_nibble_light] == hi_nibble_light:
             ans[hi_nibble_light] = hi_nibble_dark
         elif ans[hi_nibble_light] != hi_nibble_dark:
-            raise RuntimeError("Inconsistent light-dark texture relation. Can't generate a dark transition vector")
+            raise RuntimeError(common_err_msg.format(hi_nibble_dark, hi_nibble_light, hi_nibble_light, ans[hi_nibble_light]))
     
         if ans[light_cr[i]] == light_cr[i]:
             ans[light_cr[i]] = dark_cr[i]
         elif ans[light_cr[i]] != dark_cr[i]:
-            raise RuntimeError("Inconsistent light-dark texture relation. Can't generate a dark transition vector")
+            raise RuntimeError(common_err_msg.format(dark_cr[i], light_cr[i], light_cr[i], ans[dark_cr[i]]))
 
-    greyLogger.debug("transitions: {}".format(debug))
+    # greyLogger.debug("transitions: {}".format(debug))
     greyLogger.debug("end")
     return ans
 
